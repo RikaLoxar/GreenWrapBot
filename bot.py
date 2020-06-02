20b2703957a5f7a7e2bf406f21c6105cbeec64d6
RikaLoxar <aakalafat@edu.hse.ru>
2020-06-02 14:24:02 +0300

    изменил создатель на зая

--- bot.py
+++ bot.py
@@ -4,6 +4,6 @@ bot = telebot.TeleBot('1137709917:AAEZNbC7R-qspaqqdJesoDmHthFHXO0v6qw')
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет, создатель")
+    bot.reply_to(message, "Привет, зая")
 
 bot.polling()
